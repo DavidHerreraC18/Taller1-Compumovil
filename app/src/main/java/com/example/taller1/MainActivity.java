ddf4296455700c0a9da324bbc6338451ce1a5c9b
Daniel Fernandez <dafeul@hotmail.com>
2020-09-02 12:00:19 -0500

    Segunda Version Pagina Principal

--- app/src/main/java/com/example/taller1/MainActivity.java
+++ app/src/main/java/com/example/taller1/MainActivity.java
@@ -2,13 +2,56 @@ package com.example.taller1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
+    Button botonFibonacci;
+    EditText campoFibonacci;
+    Button botonFactorial;
+    Spinner spinnerFactorial;
+    Button botonPais;
+    Integer[] arregloFactorial;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        botonFibonacci = findViewById(R.id.botonFibonacci);
+        botonFibonacci.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                campoFibonacci = findViewById(R.id.campoFibonacci);
+                if(!(TextUtils.isEmpty(campoFibonacci.getText().toString()))){
+                    Toast.makeText(getBaseContext(),"Ingreso el " + campoFibonacci.getText().toString(),Toast.LENGTH_SHORT).show();
+                    //startActivity(new Intent(getBaseContext(),DetallePaseoCliente.class));
+                }else{
+                    Toast.makeText(getBaseContext(),"Aun no ingresa una posicion",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        spinnerFactorial = findViewById(R.id.spinnerFactorial);
+        initArreglo();
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(this,android.R.layout.simple_spinner_item,arregloFactorial);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerFactorial.setAdapter(adapter);
+
+    }
+
+    private void initArreglo(){
+        arregloFactorial =  new Integer[15];
+        for(int i = 0; i<arregloFactorial.length;i++){
+            arregloFactorial[i]= i+1;
+        }
     }
 }
+
